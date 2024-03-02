4dc37e62b3f1c9c2c7251a9dca413dcd75dc0c3f
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-03-02 14:41:54 +0100

    test list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { StartingGameComponent } from './starting-game/starting-game.component';
+import { GameComponent } from './game/game.component';
 
 export const routes: Routes = [
         {
         path: '',
         component: StartingGameComponent
+        }, {
+                path: 'game',
+                component: GameComponent
         }
 ];
--- src/app/starting-game/starting-game.component.ts
+++ src/app/starting-game/starting-game.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-starting-game',
@@ -9,4 +10,11 @@ import { Component } from '@angular/core';
 })
 export class StartingGameComponent {
 
+  constructor(private router: Router) {
+
+  }
+
+  newGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
