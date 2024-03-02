dedf19951d97f1edab48213afb0209849174a858
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2024-03-02 17:11:06 +0100

    added shuffle cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GameLoading } from '../../models/game-loading';
 
 @Component({
   selector: 'app-game',
@@ -9,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  gamesLoading: GameLoading | undefined;
+  pickCardAnimation = false;
 
+  constructor () {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.gamesLoading = new GameLoading();
+    console.log(this.gamesLoading);
+  }
+
+  takeCard() {
+    this.pickCardAnimation = true;
+
+  }
 }
---
+++ src/models/game-loading.ts
@@ -0,0 +1,33 @@
+import { FORMERR } from "dns";
+
+export class GameLoading {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for(let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+        shuffle(this.stack);
+    }
+}
+
+
+function shuffle(array: any) {
+    let currendIndex = array.length, temporaryValue, randomIndex;
+
+    while (0 !== currendIndex) {
+        randomIndex = Math.floor(Math.random() * currendIndex);
+        currendIndex -= 1;
+
+        temporaryValue = array[currendIndex];
+        array[currendIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
